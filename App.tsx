22eafe46f39477de7d277c860368d5aa9175f2af
alex <batmandocode@gmail.com>
2020-04-08 13:55:32 +1000

    use flex wrap to handle the inline width

--- App.tsx
+++ App.tsx
@@ -1,10 +1,72 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
+  const inputFields = [
+    {
+      title: "First Name",
+      id: "SignUpFirstName",
+      type: "text",
+      content: { value: "", errorMessage: "", errorStatus: false },
+      width: "100%",
+    },
+    {
+      title: "Last Name",
+      id: "SignUpLastName",
+      type: "text",
+      content: { value: "", errorMessage: "", errorStatus: false },
+      width: "100%",
+    },
+    {
+      title: "Email",
+      id: "SignUpEmail",
+      type: "text",
+      content: { value: "", errorMessage: "", errorStatus: false },
+      width: "100%",
+    },
+    {
+      title: "Confirm Email",
+      id: "SignUpConfirmEmail",
+      type: "text",
+      content: { value: "", errorMessage: "", errorStatus: false },
+      width: "100%",
+    },
+    {
+      title: "Password",
+      id: "SignUpPassword",
+      type: "text",
+      content: { value: "", errorMessage: "", errorStatus: false },
+      width: "100%",
+    },
+    {
+      title: "Confirm Password",
+      id: "SignUpConfirmPassword",
+      type: "text",
+      content: { value: "", errorMessage: "", errorStatus: false },
+      width: "100%",
+    },
+    {
+      title: "Confirm Password",
+      id: "SignUpConfirmPassword",
+      type: "text",
+      content: { value: "", errorMessage: "", errorStatus: false },
+      width: "50%",
+    },
+    {
+      title: "Confirm Password",
+      id: "SignUpConfirmPassword",
+      type: "text",
+      content: { value: "", errorMessage: "", errorStatus: false },
+      width: "50%",
+    },
+  ];
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+        {inputFields.map((item) => {
+          return <TextInput key={item.id} placeholder={item.title} style={{width: '100%'}}/>;
+        })}
+      </View>
     </View>
   );
 }
@@ -12,8 +74,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
