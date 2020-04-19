969d40ce7ff571bf12428db38b11aa64e540cffd
alex <batmandocode@gmail.com>
2020-04-19 14:47:24 +1000

    unfinished

--- App.tsx
+++ App.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, ScrollView, SafeAreaView, Animated } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        style={{ height: 100 }}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        scrollEventThrottle={10}
+        onScroll={() => {console.log('scrolled')}}
+      >
+        {[1, 2, 3, 4, 5].map((item, index) => {
+          return (
+            <View
+              key={index}
+              style={{
+                width: 200,
+                height: 100,
+                marginRight: 40,
+                backgroundColor: "pink",
+              }}
+            >
+              <Text>test</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
