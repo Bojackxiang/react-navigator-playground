53d96f048034876b003945f253f6b176b74b547d
alex <batmandocode@gmail.com>
2020-04-19 14:04:41 +1000

    tab bar view

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import ScrollableTabView, {
+  DefaultTabBar,
+} from "react-native-scrollable-tab-view";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <ScrollableTabView
+        tabBarUnderlineStyle={{backgroundColor: 'pink'}}
+        tabBarInactiveTextColor={"red"}
+        style={{ marginTop: 20, width: 200}}
+        initialPage={1}
+        renderTabBar={() => <DefaultTabBar />}
+      >
+        <Text tabLabel="Tab #1">some one</Text>
+        <Text tabLabel="Tab #2">favorite</Text>
+        <Text tabLabel="Tab #3">project</Text>
+      </ScrollableTabView>
     </View>
   );
 }
@@ -12,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
