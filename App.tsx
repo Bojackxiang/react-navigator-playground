8a803f816e2810563edfe7ab6028f725ccf4b5f7
Bojackxiang <batmandocode@gmail.com>
2020-04-30 13:10:11 +1000

    Added the tab view

--- App.tsx
+++ App.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Children } from "react";
+import { StyleSheet, Text, View, TextProperties } from "react-native";
+import ScrollableTabView, {
+  DefaultTabBar,
+} from "react-native-scrollable-tab-view";
 
-export default function App() {
+interface LabelTextProps extends TextProperties {
+  tabLabel: string,
+  text: string
+}
+
+const LabelText:React.SFC<LabelTextProps> = (props) => {
+  const {text, ...restProps} = props;
+
+return <Text {...restProps}>{text}</Text>
+}
+
+const App = (props: any) => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={{height: 400, width: 300, backgroundColor: "lightgreen", overflow: 'hidden' }}>
+        <ScrollableTabView
+          style={{ marginTop: 20 }}
+          initialPage={1}
+          renderTabBar={() => <DefaultTabBar />}
+        >
+          <LabelText tabLabel="Tab #1" text="test_a"/>
+          <LabelText tabLabel="Tab #2" text="test_b"/>
+          <LabelText tabLabel="Tab #3" text="test_c"/>
+        </ScrollableTabView>
+      </View>
     </View>
   );
 }
@@ -12,8 +36,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
